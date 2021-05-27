9c6e422b3f609c6d69778ab46433ae0df93077cb
Sourius <sourius@users.noreply.github.com>
2021-05-27 19:06:40 +0200

    create superclass

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from ClasificadorLDA import ClasificadorLDA
 import ImageUtils
 
 if __name__ == '__main__':
-    clf = ClasificadorLDA() # clasificador LDA
+    lda = ClasificadorLDA() # clasificador LDA
     # cargar imagenes de entrenamiento y test
     train_images, train_answers = ImageUtils.getImages('train_recortadas', True)
     test_images, test_answers = ImageUtils.getImages('test_reconocimiento', False)
 
     # entrenar
-    clf.train(train_images, train_answers)
+    lda.train(train_images, train_answers)
     
     # predecir con LDA
-    _, precision_con_lda = clf.predictAll(test_images, test_answers)
+    _, precision_con_lda = lda.predictAll(test_images, test_answers)
     print("Precision: " + str(precision_con_lda) + " %")
 
     # TODO: investigar porque falla --> solo si da tiempo
