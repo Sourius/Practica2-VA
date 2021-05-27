94a8952de680baca101bed1d8e4bc058b5ec5d6b
Sourius <sourius@users.noreply.github.com>
2021-05-27 19:08:16 +0200

    add comments

--- ClasificadorLDA.py
+++ ClasificadorLDA.py
@@ -7,11 +7,11 @@ class ClasificadorLDA(Clasificador):
     #constructor
     def __init__(self):
         lda = LinearDiscriminantAnalysis() # lda
-        
+        # utilizar vector de caracteristicas hog
         win_size = (Constants.HOG_WIN_SIZE, Constants.HOG_WIN_SIZE) # tamaño de la imagen
         block_size = (Constants.HOG_BLOCK_SIZE, Constants.HOG_BLOCK_SIZE) # tamaño del bloque
         block_stride = (Constants.HOG_BLOCK_STRIDE, Constants.HOG_BLOCK_STRIDE) # tamaño de desplazamiento entre los bloques
         cell_size = (Constants.HOG_CELL_SIZE, Constants.HOG_CELL_SIZE) # tamaño de las celdas
         hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, Constants.HOG_N_BINS) # descriptor hog
-        
+        # crear clasificador
         Clasificador.__init__(self, lda, hog, None)
