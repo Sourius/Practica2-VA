d3014aa88a59f80e3da6eb39296b4b276552c4ab
Sourius <sourius@users.noreply.github.com>
2021-05-28 23:17:13 +0200

    more classifiers + update main

--- Clasificador_KNN_PCA_LBP.py
+++ Clasificador_KNN_PCA_LBP.py
@@ -1,31 +1,25 @@
-import cv2
-from sklearn.decomposition import PCA
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.decomposition import PCA
 from Clasificador import Clasificador
 import Constants
 
-class Clasificador_KNN_PCA_HOG(Clasificador):
+class Clasificador_KNN_PCA_LBP(Clasificador):
     def __init__(self, n):
         # clasificador KNN
         knn = KNeighborsClassifier(n_neighbors=Constants.N_NEIGHBOURS)
-		
-		# reduccon de dimensionalidad con PCA
-        pca = PCA(n, svd_solver='full')
         
-		# utilizar vector de caracteristicas hog
-        win_size = (Constants.HOG_WIN_SIZE, Constants.HOG_WIN_SIZE)  # tamaño de la imagen
-        block_size = (Constants.HOG_BLOCK_SIZE, Constants.HOG_BLOCK_SIZE)  # tamaño del bloque
-        block_stride = (Constants.HOG_BLOCK_STRIDE, Constants.HOG_BLOCK_STRIDE)  # tamaño de desplazamiento entre los bloques
-        cell_size = (Constants.HOG_CELL_SIZE, Constants.HOG_CELL_SIZE)  # tamaño de las celdas
-        hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, Constants.HOG_N_BINS)  # descriptor hog
-		
-        super().__init__(knn, pca, hog)
+        # reduccon de dimensionalidad con PCA
+        pca = PCA(n, svd_solver='full')
+
+        # utilizar vector de caracteristicas lbp
+		# clasificador KNN_PCA_LBP
+        super().__init__(knn, pca, None)
 
     # devuelve los vectores de caracteristicas de varias imagenes
     # recibe imgs, una array con las imagenes
     def getEigenVectors(self, img):
-        return self._getHOGEigenVectors(img)
-
+        return self._getLBPEigenVectors(img)
+    
     # entrena el clasificador con las imagenes de entenamiento
     # recibe los valores reducidos de las imagenes y sus valores de clasificacion
     def train(self, data_list, answers):
