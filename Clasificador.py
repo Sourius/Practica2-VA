c3caec88fb35672a137de0bcdaa000da388afece
chris-bena <58047436+chris-bena@users.noreply.github.com>
2021-05-28 23:22:23 +0200

    Revert "code cleanup and update main with options"
    This reverts commit d473fb2da581a5628d35058006f8a6e322af7a02.

--- Clasificador.py
+++ Clasificador.py
@@ -34,7 +34,7 @@ class Clasificador:
         pass
 
     # devuelve los vectores de caracteristicas de varias imagenes
-    # recibe imgs, una array con las imagenes redimensionadas
+    # recibe imgs, una array con las imagenes
     def getEigenValuesAll(self, imgs):
         eigen_vectors_list = []
         for img in imgs:
@@ -44,7 +44,8 @@ class Clasificador:
         return np.array(eigen_vectors_list)
 
     # devuelve los valores reducidos de vector de caracteristicas
-    # recibe las lista de los vectores de caracteristicasy los valores de clasificación
+    # recibe las lista de los vectores de caracteristicas
+    # recibe la lista de valores de clasificación
     def _reduceValues(self, eigen_vectors_list, answers):
         if answers is not None:
             return self.reductor.fit_transform(eigen_vectors_list, answers)
@@ -52,27 +53,25 @@ class Clasificador:
             return self.reductor.transform(eigen_vectors_list)
 
     # entrena el clasificador con las imagenes de entenamiento
-    # recibe la lista de vectores de caracteristicas y sus valores de clasificacion
+    # recibe las imagenes y sus valores de clasificacion
     def train(self, eigen_vectors, answers):
         pass
 
     # entrena el clasificador con las imagenes de entenamiento
-    # recibe los valores reducidos y los valores de clasificación
+    # recibe las imagenes y sus valores de clasificacion
     def _train(self, reduced_values, answers):
         self.clasificador.fit(reduced_values, answers) 
 
     # devuelve las prediccion de una imagen
-    # recibe la imagen
+    # recibe los valores reducidos de la imagen
     def predict(self, data):
         preds = self.predictAll([data])
         return preds[0]
 
     # devuelve las predicciones de las imagenes
-    # recibe las imagenes
+    # recibe los valores reducidos de las imagenes
     def predictAll(self, imgs):
         pass
 
-	# devuelve las predicciones de las imagenes
-    # recibe los valores reducidos de las imagenes
     def _predictAll(self, reduced_values):
         return self.clasificador.predict(reduced_values)
