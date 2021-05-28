489ed62ba51d946dd1aae4ef475201147d8e4648
chris-bena <58047436+chris-bena@users.noreply.github.com>
2021-05-28 23:26:44 +0200

    Revert "code refactor"
    This reverts commit e15e82511db0b48910690b0215f8a19d0f1f50e0.

--- Clasificador_KNN_LDA_LBP.py
+++ Clasificador_KNN_LDA_LBP.py
@@ -1,6 +1,9 @@
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.neighbors import KNeighborsClassifier
 from Clasificador import Clasificador
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from skimage.feature import local_binary_pattern
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+
 
 class Clasificador_KNN_LDA_LBP(Clasificador):
     def __init__(self, k):
@@ -12,14 +15,17 @@ class Clasificador_KNN_LDA_LBP(Clasificador):
     # devuelve el vector de caracteristicas de la imagen
     # recibe la imagen redimensionada
     def getEigenVectors(self, img):
-        return self._getLBPEigenVectors(img)
+        eigen_vectors = local_binary_pattern(img, 8, 4).flatten()
+        eigen_vectors = np.nan_to_num(np.array(eigen_vectors))
+        return eigen_vectors
 
     def train(self, data_list, answers):
         eigen_vectors = self.getEigenValuesAll(data_list)
-        reduced_values = self._reduceValues(eigen_vectors, answers)
-        self._train(reduced_values, answers) 
+        self.reductor.fit(eigen_vectors, answers)
+        reduced_values = self.reductor.transform(eigen_vectors)
+        self.clasificador.fit(reduced_values, answers)   
 
     def predictAll(self, data_list):
         eigen_vectors = self.getEigenValuesAll(data_list)
-        reduced_values = self._reduceValues(eigen_vectors, None)
-        return self._predictAll(reduced_values)
+        reduced_values = self.reductor.transform(eigen_vectors)
+        return self.clasificador.predict(reduced_values)
--- Clasificador_KNN_PCA_HOG.py
+++ Clasificador_KNN_PCA_HOG.py
@@ -1,13 +1,15 @@
-import cv2
 from sklearn.decomposition import PCA
 from sklearn.neighbors import KNeighborsClassifier
 from Clasificador import Clasificador
+import cv2
+
 import Constants
+import numpy as np
 
 class Clasificador_KNN_PCA_HOG(Clasificador):
     def __init__(self):
         # clasificador KNN
-        knn = KNeighborsClassifier(n_neighbors=16)
+        knn = KNeighborsClassifier(n_neighbors=4)
 
         # utilizar vector de caracteristicas hog
         win_size = (Constants.HOG_WIN_SIZE, Constants.HOG_WIN_SIZE)  # tamaño de la imagen
@@ -23,18 +25,22 @@ class Clasificador_KNN_PCA_HOG(Clasificador):
     # devuelve los vectores de caracteristicas de varias imagenes
     # recibe imgs, una array con las imagenes
     def getEigenVectors(self, img):
-        return self._getHOGEigenVectors(img)
+        eigen_vectors = self.descriptor.compute(img).flatten()
+        eigen_vectors = np.nan_to_num(np.array(eigen_vectors))
+        return eigen_vectors
 
     # entrena el clasificador con las imagenes de entenamiento
     # recibe los valores reducidos de las imagenes y sus valores de clasificacion
     def train(self, data_list, answers):
-        eigen_vectors_list = self.getEigenValuesAll(data_list)
-        reduced_values = self._reduceValues(eigen_vectors_list, answers)
-        self._train(reduced_values, answers)
+        eigen_values_list = self.getEigenValuesAll(data_list)
+        self.reductor.fit(eigen_values_list, answers)
+        reduced_values = self.reductor.transform(eigen_values_list)
+        self.clasificador.fit(reduced_values, answers)
 
     # devuelve las predicciones de las imagenes
     # recibe los valores reducidos de las imagenes
     def predictAll(self, data_list):
         eigen_values_list = self.getEigenValuesAll(data_list)
-        reduced_values = self._reduceValues(eigen_values_list, None)
-        return self._predictAll(reduced_values)
+        reduced_values = self.reductor.transform(eigen_values_list)
+        predictions = self.clasificador.predict(reduced_values)
+        return predictions
