52a38564f29525587828bdf49b18bb907c3f3926
chris-bena <58047436+chris-bena@users.noreply.github.com>
2021-05-28 23:22:11 +0200

    Revert "more classifiers + update main"
    This reverts commit d3014aa88a59f80e3da6eb39296b4b276552c4ab.

--- Clasificador_KNN_PCA_HOG.py
+++ Clasificador_KNN_PCA_HOG.py
@@ -5,20 +5,19 @@ from Clasificador import Clasificador
 import Constants
 
 class Clasificador_KNN_PCA_HOG(Clasificador):
-    def __init__(self, n):
+    def __init__(self):
         # clasificador KNN
         knn = KNeighborsClassifier(n_neighbors=Constants.N_NEIGHBOURS)
-		
-		# reduccon de dimensionalidad con PCA
-        pca = PCA(n, svd_solver='full')
-        
-		# utilizar vector de caracteristicas hog
+
+        # utilizar vector de caracteristicas hog
         win_size = (Constants.HOG_WIN_SIZE, Constants.HOG_WIN_SIZE)  # tamaño de la imagen
         block_size = (Constants.HOG_BLOCK_SIZE, Constants.HOG_BLOCK_SIZE)  # tamaño del bloque
         block_stride = (Constants.HOG_BLOCK_STRIDE, Constants.HOG_BLOCK_STRIDE)  # tamaño de desplazamiento entre los bloques
         cell_size = (Constants.HOG_CELL_SIZE, Constants.HOG_CELL_SIZE)  # tamaño de las celdas
         hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, Constants.HOG_N_BINS)  # descriptor hog
-		
+
+        # reduccon de dimensionalidad con PCA
+        pca = PCA(121, svd_solver='full')
         super().__init__(knn, pca, hog)
 
     # devuelve los vectores de caracteristicas de varias imagenes
--- main.py
+++ main.py
@@ -1,62 +1,48 @@
 from Clasificador_LDA_LDA_HOG import Clasificador_LDA_LDA_HOG
 from Clasificador_LDA_LDA_LBP import Clasificador_LDA_LDA_LBP
 from Clasificador_LDA_PCA_HOG import Clasificador_LDA_PCA_HOG
-from Clasificador_LDA_PCA_LBP import Clasificador_LDA_PCA_LBP
+from Clasificador_KNN_PCA_HOG import Clasificador_KNN_PCA_HOG
 from Clasificador_KNN_LDA_LBP import Clasificador_KNN_LDA_LBP
 from Clasificador_KNN_LDA_HOG import Clasificador_KNN_LDA_HOG
-from Clasificador_KNN_PCA_HOG import Clasificador_KNN_PCA_HOG
-from Clasificador_KNN_PCA_LBP import Clasificador_KNN_PCA_LBP
 
 import argparse
 import ImageUtils
 import Clasificador
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser( description='Entrena sober train y ejecuta el clasificador sobre imgs de test')
-    parser.add_argument('--train_path', type=str, default="train_recortadas", help='Path al directorio de imgs de train')
-    parser.add_argument('--test_path', type=str, default="test_reconocimiento", help='Path al directorio de imgs de test')
-    parser.add_argument('--classifier', type=str, default="lda_lda_hog", help='String con el nombre del clasificador')
+    # cargar imagenes de entrenamiento y test
+    train_images, train_answers = ImageUtils.getImages('train_recortadas', True)
+    test_images, test_answers = ImageUtils.getImages('test_reconocimiento', False)
+    
+    parser = argparse.ArgumentParser(description='Trains and executes a given classifier over a set of testing images')
+    parser.add_argument('--clasificador', default="clasificador_lda_lda_hog", help='Classifier string name')
 
     # obtener los argumentos
     args = parser.parse_args()
 
-    # cargar y procesar imagenes de entrenamiento y test
-    train_images, train_answers = ImageUtils.getImages(args.train_path, True)
-    test_images, test_answers = ImageUtils.getImages(args.test_path, False)
+    clasificador = args.clasificador.lower()
+    print(clasificador.upper())
 
-    #Tratamiento de los datos
-    # Crear el clasificador 
-    classifier_name = args.classifier.lower()
-    print(classifier_name.upper())
-
-    if classifier_name == "lda_lda_hog":
+    if(clasificador == "clasificador_lda_lda_hog"):
         clf = Clasificador_LDA_LDA_HOG() # clasificador LDA_LDA_HOG
-    elif classifier_name == "lda_lda_lbp":
+        
+    if(clasificador == "clasificador_lda_lda_lbp"):
         clf = Clasificador_LDA_LDA_LBP() # clasificador LDA_LDA_LBP
-    elif classifier_name == "lda_pca_hog":
+
+    if(clasificador == "clasificador_lda_pca_hog"):
         n = min(len(train_images), len(test_images)) 
         clf = Clasificador_LDA_PCA_HOG(n)# clasificador LDA_PCA_HOG
-    elif classifier_name == "lda_pca_lbp":
-        n = min(len(train_images), len(test_images)) 
-        clf = Clasificador_LDA_PCA_LBP(n)# clasificador LDA_PCA_LBP
-    elif classifier_name == "knn_lda_hog":
-        clf = Clasificador_KNN_LDA_HOG()# clasificador KNN_LDA_HOG
-    elif classifier_name == "knn_lda_lbp":
-        clf = Clasificador_KNN_LDA_LBP()# clasificador KNN_LDA_LBP
-    elif classifier_name == "knn_pca_hog":
-        n = min(len(train_images), len(test_images)) 
-        clf = Clasificador_KNN_PCA_HOG(n)# clasificador KNN_PCA_HOG
-    elif  classifier_name == "knn_pca_lbp":
-        n = min(len(train_images), len(test_images)) 
-        clf = Clasificador_KNN_PCA_LBP(n)# clasificador KNN_PCA_LBP
-    else:
-        raise ValueError('Tipo de clasificador incorrecto')        
-    
-    # Entrenar el clasificador
-    clf.train(train_images, train_answers)# entrenamiento
 
+    if(clasificador == "clasificador_knn_lda_hog"):
+        clf = Clasificador_KNN_LDA_HOG()# clasificador KNN_LDA_HOG
+    
+    if(clasificador == "clasificador_knn_lda_lbp"):
+        clf = Clasificador_KNN_LDA_LBP()# clasificador KNN_LDA_LBP
+    
+    if(clasificador == "clasificador_knn_pca_hog"):
+        clf = Clasificador_KNN_PCA_HOG()# clasificador KNN_PCA_HOG
+    
+    clf.train(train_images, train_answers)# entrenamiento
     predicts = clf.predictAll(test_images)# predección
     precision = Clasificador.getStats(test_answers, predicts)
     print("Precision: " + str(precision) + " %")
-
-    # Guardar los resultados en ficheros de texto
