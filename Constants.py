e3234a91b90c3f2758152fbb94fae2ef34372138
Sourius <sourius@users.noreply.github.com>
2021-05-29 15:27:16 +0200

    update constants

--- Constants.py
+++ Constants.py
@@ -9,8 +9,9 @@ HOG_CELL_SIZE = 3
 HOG_N_BINS = 9
 # parametros para el clasificador KNN
 N_NEIGHBOURS = 4
-RESULTS_FILE = "resultado.txt"  # fichero de texto donde se guardan las detecciones
-RESULTS_DIR = "resultados"
 # parametros para lbp
 LBP_NEIGHBOURS = 8
 LBP_RADIUS = 4
+# generacion de resultados
+RESULTS_FILE = "resultado.txt"  # fichero de texto donde se guardan las detecciones
+RESULTS_DIR = "resultados"
